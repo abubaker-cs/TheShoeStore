0b430adb5623ec7f944c01fa7f29bf52074c9c93
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-06-22 13:10:41 +0500

    Created ShoeListAdapter.kt for the RecyclerView being used in the ShoeListFragment.kt

--- app/src/main/java/org/abubaker/shoesplanet/ui/adapter/ShoeListAdapter.kt
+++ app/src/main/java/org/abubaker/shoesplanet/ui/adapter/ShoeListAdapter.kt
@@ -1,3 +1,76 @@
 package org.abubaker.shoesplanet.ui.adapter
 
-class ShoeListAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import org.abubaker.shoesplanet.databinding.ListItemShoeBinding
+import org.abubaker.shoesplanet.model.Shoe
+
+/**
+ * FILE 06
+ *
+ * Adapter for the RecyclerView being used in the ShoeListFragment.kt file
+ */
+class ShoeListAdapter(
+    private val clickListener: (Shoe) -> Unit
+) : ListAdapter<Shoe, ShoeListAdapter.ShoeViewHolder>(DiffCallback) {
+
+    //
+    class ShoeViewHolder(
+        private var binding: ListItemShoeBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        //
+        fun bind(shoe: Shoe) {
+            binding.shoe = shoe
+            binding.executePendingBindings()
+        }
+
+    }
+
+    //
+    companion object DiffCallback : DiffUtil.ItemCallback<Shoe>() {
+
+        //
+        override fun areItemsTheSame(oldItem: Shoe, newItem: Shoe): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        //
+        override fun areContentsTheSame(oldItem: Shoe, newItem: Shoe): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    //
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoeViewHolder {
+
+        //
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        //
+        return ShoeViewHolder(
+            ListItemShoeBinding.inflate(layoutInflater, parent, false)
+        )
+
+    }
+
+    //
+    override fun onBindViewHolder(holder: ShoeViewHolder, position: Int) {
+
+        //
+        val shoe = getItem(position)
+
+        //
+        holder.itemView.setOnClickListener {
+            clickListener(shoe)
+        }
+
+        //
+        holder.bind(shoe)
+    }
+
+}
