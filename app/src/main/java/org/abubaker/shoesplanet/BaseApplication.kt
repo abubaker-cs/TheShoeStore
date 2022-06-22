79aff8be1a19d0036e068aea9af6ea63267d39ce
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-06-22 12:02:49 +0500

    Working on the BaseApplication.kt

--- app/src/main/java/org/abubaker/shoesplanet/BaseApplication.kt
+++ app/src/main/java/org/abubaker/shoesplanet/BaseApplication.kt
@@ -1,3 +1,26 @@
 package org.abubaker.shoesplanet
 
-class BaseApplication
+import android.app.Application
+import org.abubaker.shoesplanet.data.ShoeDatabase
+
+/**
+ * File 05
+ *
+ * An application class that inherits from [Application], allows for the creation of a singleton
+ * instance of the [ShoeDatabase]
+ *
+ */
+
+// We will instantiate the database instance in the Application class
+class BaseApplication : Application() {
+
+    // Use lazy delegate so the instance database is lazily created when you first need/access the
+    // reference (rather than when the app starts) .
+    val database: ShoeDatabase by lazy {
+
+        //  Instantiate the database instance by calling getDatabase() on ShoeDatabase passing in the context
+        ShoeDatabase.getDatabase(this)
+
+    }
+
+}
