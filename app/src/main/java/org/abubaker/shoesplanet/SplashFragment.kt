5aec2cf17e178e8acc9f2369682b4555d5a47c7c
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-03 00:18:15 +0500

    Working on login/splash fragments.

---
+++ app/src/main/java/org/abubaker/shoesplanet/SplashFragment.kt
@@ -0,0 +1,46 @@
+package org.abubaker.shoesplanet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import org.abubaker.shoesplanet.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(1500)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    findNavController().navigate(
+                        R.id.action_splashFragment_to_signInFragment
+                    )
+                }
+            }
+        }
+        thread.start()
+
+    }
+
+
+}
