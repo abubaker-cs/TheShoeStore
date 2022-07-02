48a8e9451f7e75b7c520acebbd4337a3a00c6e6b
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-03 01:21:40 +0500

    Working on login/splash fragments.

--- app/src/main/java/org/abubaker/shoesplanet/SplashFragment.kt
+++ app/src/main/java/org/abubaker/shoesplanet/SplashFragment.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.abubaker.shoesplanet.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    private val activityScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,20 +31,30 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val thread: Thread = object : Thread() {
-            override fun run() {
-                try {
-                    sleep(1500)
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
-                } finally {
-                    findNavController().navigate(
-                        R.id.action_splashFragment_to_signInFragment
-                    )
-                }
-            }
+        activityScope.launch {
+            delay(1500)
+
+            findNavController().navigate(
+                R.id.action_splashFragment_to_signInFragment
+            )
+
         }
-        thread.start()
+
+
+//        val thread: Thread = object : Thread() {
+//            override fun run() {
+//                try {
+//                    sleep(1500)
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                } finally {
+//                    findNavController().navigate(
+//                        R.id.action_splashFragment_to_signInFragment
+//                    )
+//                }
+//            }
+//        }
+//        thread.start()
 
     }
 
