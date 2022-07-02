b725285b5ff0a061acc64e232a4a4f4669d44863
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-02 18:21:28 +0500

    Working...

--- app/src/main/java/org/abubaker/shoesplanet/ui/AddShoeFragment.kt
+++ app/src/main/java/org/abubaker/shoesplanet/ui/AddShoeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -112,6 +113,9 @@ class AddShoeFragment : Fragment() {
                 R.id.action_addShoeFragment_to_shoeListFragment
             )
 
+        } else {
+            Toast.makeText(requireContext(), "Please provide complete data.", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
