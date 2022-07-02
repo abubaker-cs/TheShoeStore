a2fbd6297c2b00fd819fd9620fac558f154976df
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-03 02:58:28 +0500

    Implemented functionality for logout (menu)

--- app/src/main/java/org/abubaker/shoesplanet/MainActivity.kt
+++ app/src/main/java/org/abubaker/shoesplanet/MainActivity.kt
@@ -76,10 +76,14 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_logout -> true
-            else -> super.onOptionsItemSelected(item)
+
+        if (item.itemId == R.id.action_logout) {
+            val navController = findNavController(R.id.nav_host_fragment)
+            navController.navigate(R.id.signInFragment)
+            return true
         }
+
+        return super.onOptionsItemSelected(item)
 
     }
 
