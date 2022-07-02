a5dd9778be573079662cb15cae82378b3a35693c
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-02 23:11:50 +0500

    Working on login/splash fragments.

--- app/src/main/java/org/abubaker/shoesplanet/Splash.kt
+++ app/src/main/java/org/abubaker/shoesplanet/Splash.kt
@@ -1,11 +1,34 @@
 package org.abubaker.shoesplanet
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class Splash : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } finally {
+                    startActivity(Intent(this@Splash, SignIn::class.java))
+                }
+            }
+        }
+        thread.start()
+
     }
+
 }
