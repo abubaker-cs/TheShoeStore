8e3cd1cf1b068d6e4971a06b5184fa7ab1623d7f
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-03 15:35:28 +0500

    Implemented circular progress bar on the splash screen.

--- app/src/main/java/org/abubaker/shoesplanet/onboarding/SplashFragment.kt
+++ app/src/main/java/org/abubaker/shoesplanet/onboarding/SplashFragment.kt
@@ -1,11 +1,13 @@
 package org.abubaker.shoesplanet.onboarding
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -27,37 +29,40 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.circularProgressBar.apply {
+            // Set Progress
+            progress = 0f
 
+            // Set Progress Max
+            progressMax = 100f
+            setProgressWithAnimation(100f, 2500)
+            progressBarWidth = 15f
+            backgroundProgressBarWidth = 16f
+            // progressBarColor = Color.GREEN
+            progressBarColorStart = Color.GREEN
+            progressBarColorEnd = Color.BLUE
+            progressBarColorDirection = CircularProgressBar.GradientDirection.LEFT_TO_RIGHT
+
+            backgroundProgressBarColor = Color.WHITE
+
+            roundBorder = true
+
+        }
 
         activityScope.launch {
-            delay(3000)
+            delay(2550)
 
             findNavController().navigate(
                 R.id.action_splashFragment_to_signInFragment
             )
 
         }
-
-
-//        val thread: Thread = object : Thread() {
-//            override fun run() {
-//                try {
-//                    sleep(1500)
-//                } catch (e: InterruptedException) {
-//                    e.printStackTrace()
-//                } finally {
-//                    findNavController().navigate(
-//                        R.id.action_splashFragment_to_signInFragment
-//                    )
-//                }
-//            }
-//        }
-//        thread.start()
 
     }
 
