5135ea0c7c20c2c5fb368ef3700b4ca26602dfbd
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-06 02:29:24 +0500

    Adding comments

--- app/src/main/java/org/abubaker/shoesplanet/ui/ShoeDetailsFragment.kt
+++ app/src/main/java/org/abubaker/shoesplanet/ui/ShoeDetailsFragment.kt
@@ -16,40 +16,38 @@ import org.abubaker.shoesplanet.model.Shoe
 import org.abubaker.shoesplanet.ui.viewmodel.ShoeViewModel
 import org.abubaker.shoesplanet.ui.viewmodel.ShoeViewModelFactory
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ShoeDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShoeDetailsFragment : Fragment() {
 
+    // This reference will be used to retrieve the ID, so information about correct record can be displayed.
     private val navigationArgs: ShoeDetailsFragmentArgs by navArgs()
-
-    // TO DO: Refactor the creation of the view model to take an instance of
-    //  shoeViewModelFactory. The factory should take an instance of the Database retrieved
-    //  from BaseApplication
-
-    //
-    private val viewModel: ShoeViewModel by activityViewModels {
-        ShoeViewModelFactory(
-            (activity?.application as BaseApplication).database.shoeDao()
-        )
-    }
 
     private lateinit var shoe: Shoe
 
     private var _binding: FragmentShoeDetailsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
+
+    // Get reference to our viewModel i.e. ShoeViewModel
+    private val viewModel: ShoeViewModel by activityViewModels {
+
+        // The ShoeViewModelFactory will take an instance of the Database retrieved from BaseApplication
+        ShoeViewModelFactory(
+            (activity?.application as BaseApplication).database.shoeDao()
+        )
+
+    }
+
+    /**
+     * Inflate XML Layout
+     */
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        // _binding = FragmentShoeDetailsBinding.inflate(inflater, container, false)
+
+        // Inflate: @layout/fragment_shoe_details.xml
         _binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_details, container, false)
         return binding.root
@@ -57,16 +55,20 @@ class ShoeDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // We are ID of shoe record, that was clicked by the user in the "List Fragment",
+        // This reference will be helpful for passing the information and allowing the user
+        // to modify correct record.
         val id = navigationArgs.id
 
-        // T ODO: Observe a shoe that is retrieved by id, set the shoe variable,
-        //  and call the bind shoe method
-
+        // This is an observer method, we will use it to pass the user's provided data and start
+        // the storage process.
         viewModel.getShoe(id).observe(this.viewLifecycleOwner) { selectedShoe ->
 
+            // Store the user provide data in the shoe variable
             shoe = selectedShoe
 
-            //
+            // Pass in the user's provided data, and initialize the bindShoe() function
             bindShoe(shoe)
 
         }
@@ -74,29 +76,52 @@ class ShoeDetailsFragment : Fragment() {
 
     }
 
+    // This function will be used to store information
     private fun bindShoe(shoe: Shoe) {
 
+        // Shorthand, i.e. the binding wrapper
         binding.apply {
-            shoeModel.text = shoe.modelNumber
-            brandName.text = shoe.brandName
-            shoePrice.text = shoe.shoePrice
-            shoeType.text = shoe.shoeType
-            shoeColor.text = shoe.shoeColor
-            shoeSize.text = shoe.shoeSize
-            inStock.text = shoe.inStock.toString()
 
+            // Shoe Model Number
+            shoeModel.text = shoe.modelNumber
+
+            // Shoe Designer or Brand
+            brandName.text = shoe.brandName
+
+            // Price
+            shoePrice.text = shoe.shoePrice
+
+            // Shoe Type
+            shoeType.text = shoe.shoeType
+
+            // Color
+            shoeColor.text = shoe.shoeColor
+
+            // Shoe Size
+            shoeSize.text = shoe.shoeSize
+
+            // In Stock?
+            // Conditional : We need to first check if the user has selected "in stock" option
+            inStock.text = shoe.inStock.toString()
             if (shoe.inStock) {
                 inStock.text = getString(R.string.in_stock)
             } else {
                 inStock.text = getString(R.string.out_of_stock)
             }
 
+            // Notes
             shoeNotes.text = shoe.notes
 
+            // FAB: Edit Button
             editShoeFab.setOnClickListener {
+
+                // Navigate the user to the (@layout/fragment_add_shoe.xml) for editing the existing
+                // record. I will be using the same "Add Fragment", by replacing the "Add Button" with
+                // Save and Delete options (buttons).
                 val action =
                     ShoeDetailsFragmentDirections.actionShoeDetailsFragmentToAddShoeFragment(shoe.id)
                 findNavController().navigate(action)
+
             }
 
         }
