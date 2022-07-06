7d1c9dec357505b894ae169ebd287af8934090af
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-06 15:33:35 +0500

    Adding comments in SignInFragment.kt and SignUpFragment.kt

--- app/src/main/java/org/abubaker/shoesplanet/data/SlidesRecord.kt
+++ app/src/main/java/org/abubaker/shoesplanet/data/SlidesRecord.kt
@@ -6,21 +6,25 @@ import org.abubaker.shoesplanet.model.Slide
 object SlidesRecord {
 
     val Slides = listOf(
+
+        // Slide #1
         Slide(
             R.drawable.shoe01,
-            "Title 01",
+            "Instruction #1",
             " incididunt ut labore et dolore magna aliqua ipsum \ndolor sit amet, consectetur adipi"
         ),
 
+        // Slide #2
         Slide(
             R.drawable.shoe02,
-            "Title 02",
+            "Instruction #2",
             "Lorem ipsum dolor sit amet, consectetur adipi \nscing elit, sed do eiusmod tempor amet."
         ),
 
+        // Slide #3
         Slide(
             R.drawable.shoe03,
-            "Title 03",
+            "Instruction #3",
             "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat nostrud"
         )
 
