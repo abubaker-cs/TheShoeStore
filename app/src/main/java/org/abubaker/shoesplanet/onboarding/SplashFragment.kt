cfc967ac7314a3b40d683cab989be919bf3a2d4b
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-07-06 15:20:33 +0500

    Adding comments in ShoeListAdapter.kt

--- app/src/main/java/org/abubaker/shoesplanet/onboarding/SplashFragment.kt
+++ app/src/main/java/org/abubaker/shoesplanet/onboarding/SplashFragment.kt
@@ -23,43 +23,68 @@ class SplashFragment : Fragment() {
 
     private val activityScope = CoroutineScope(Dispatchers.Main)
 
+    // Inflate XML file:
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        // _binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        // @layout/fragment_splash.xml
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
         return binding.root
 
     }
 
+    /**
+     * We will configure the settings for CircularProgressBar in onViewCreated() lifecycle event.
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Shorthand: Binding wrapper
+        //
+        // I am using 3rd party library: https://github.com/lopspower/CircularProgressBar for using
+        // Circular Progress bar in the Splash screen @layout/fragment_splash.xml
         binding.circularProgressBar.apply {
-            // Set Progress
+
+            // Starting position of the animation
             progress = 0f
 
             // Set Progress Max.
             progressMax = 100f
+
+            // Circular Bar will take 2.5 seconds to complete 100 frames (100%)
             setProgressWithAnimation(100f, 2500)
+
+            // This will be animated width of the bar, starting with Green and ending with Blue colors.
             progressBarWidth = 20f
+
+            // Base width of the bar (the one with the white background color)
             backgroundProgressBarWidth = 15f
-            // progressBarColor = Color.GREEN
+
+            // Initial Color
             progressBarColorStart = Color.GREEN
+
+            // Ending Color
             progressBarColorEnd = Color.BLUE
+
+            // Animation Starting Direction: Left to Right
             progressBarColorDirection = CircularProgressBar.GradientDirection.LEFT_TO_RIGHT
 
+            // Background color
             backgroundProgressBarColor = Color.WHITE
 
+            // Rounded Borders for the Bar
             roundBorder = true
 
         }
 
         activityScope.launch {
+
+            // Wait for 2.55 seconds
             delay(2550)
 
+            // TODO Then navigate the user to the next fragment.
             findNavController().navigate(
                 R.id.action_splashFragment_to_onboardingFragment
             )
